a45243c267719aa032fdfc3e6a5329b52546175e
Puneeth Rai <npuneethrai@yahoo.co.in>
2016-03-24 16:22:04 +0530

    angular-long-press.js

---
+++ angular-long-press.js
@@ -0,0 +1,56 @@
+(function() {
+    'use strict';
+    angular
+        .module('pr.longpress')
+        .directive('onLongPress', function ($timeout) {
+            return {
+                restrict: 'A',
+                link: function ($scope, $elm, $attrs) {
+                    var timer;
+                    $elm.bind('touchstart, mousedown', onEnter);
+    
+                    $elm.bind('touchend, mouseup', onExit);
+                    $elm.bind('click', onClick);
+    
+                    function onEnter(evt) {
+                        $timeout.cancel(timer);
+                        //To handle click event properly
+                        $scope.longPressSent = false;
+                        // We'll set a timeout for 600 ms for a long press
+                        timer = $timeout(function () {
+                            $scope.longPressSent = true;
+                            // If the touchend event hasn't fired,
+                            // apply the function given in on the element's on-long-press attribute
+                            $scope.$apply(function () {
+                                $scope.$eval($attrs.onLongPress);
+                            });
+                        }, 600);
+    
+                    }
+    
+                    function onExit(evt) {
+                        // Prevent the onLongPress event from firing
+                        $timeout.cancel(timer);
+                        // If there is an on-touch-end function attached to this element, apply it
+                        if ($attrs.onTouchEnd) {
+                            $scope.$apply(function () {
+                                $scope.$eval($attrs.onTouchEnd);
+                            });
+                        }
+    
+                    }
+    
+                    function onClick(evt) {
+                        //If long press is handled then prevent click
+                        if ($scope.longPressSent && (!$attrs.preventClick || $attrs.preventClick === "true")) {
+                            evt.preventDefault();
+                            evt.stopPropagation();
+                            evt.stopImmediatePropagation();
+                        }
+    
+                    }
+                }
+            };
+        });
+    
+})();
